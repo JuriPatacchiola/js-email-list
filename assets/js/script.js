df809f697c93edef7408418072d34f2e4ee26159
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-23 16:06:43 +0200

    add the different cost i need to create the program for js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const API_URL = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const btn = document.getElementById('generate');
+const list = document.getElementById('emails');
+const errorBox = document.getElementById('error');
+const howMany = document.getElementById('howMany');
+const status = document.getElementById('status');
