998f3fbee678906b9df4e02014ce13195b11cefc
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-23 16:11:43 +0200

    add the event for the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ const list = document.getElementById('emails');
 const errorBox = document.getElementById('error');
 const howMany = document.getElementById('howMany');
 const status = document.getElementById('status');
+
+// qui metto l'evento del click per il bottone
+btn.addEventListener('click', function () {
+// qui mi metto a pulire la lista e azzero eventuali messaggi di errore o di stato
+list.innerHTML = '';
+errorBox.textContent = '';
+status.textContent = 'Caricamento...';
+// qui ho disabilitato il bottone quando sta lavorando per creare le email 
+btn.disabled = true; 
+});
